f723b32cbed37e833b3462efd28cb368e9e0e978
Itsharshu143 <116162890+Itsharshu143@users.noreply.github.com>
2025-01-29 15:25:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('AUTH_CHANNEL', '-10
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://harshumanal:harshumanal@cluster0.jxxdp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://harshu1:harshu1@cluster0.7fdwy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://harshumanal:harshumanal@cluster0.jxxdp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
